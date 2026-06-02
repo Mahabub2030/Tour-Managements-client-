6d19e64b5cf8650db04b2b076d62592f18d0f18f
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2026-06-02 20:34:00 +0300

    added navber footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CommonLayout from "./components/layout/CommonLayout";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div>
       <CommonLayout>
         <Outlet />
       </CommonLayout>
---
+++ src/components/modules/homepage/HeroSections.tsx
@@ -0,0 +1,3 @@
+export default function HeroSections() {
+  return <div>HeroSections</div>;
+}
